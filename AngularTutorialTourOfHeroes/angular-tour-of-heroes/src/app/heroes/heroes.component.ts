258825341ed79bcbd7e7c6e9cf2b9767e7948269
Matthias Zeleny <matthias.zeleny@andrena.de>
2018-11-19 12:56:45 +0100

    show selected hero in detail and allow changing the name

--- AngularTutorialTourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ AngularTutorialTourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -16,6 +16,11 @@ export class HeroesComponent implements OnInit {
   
   heroes = HEROES;
 
+  selectedHero: Hero;
+  onSelect(hero: Hero){
+    this.selectedHero = hero;
+  }
+
   ngOnInit() {
   }
 
