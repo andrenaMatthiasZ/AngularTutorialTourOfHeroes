8f4906e73180500e9db119947505602e74dc5d25
Matthias Zeleny <matthias.zeleny@andrena.de>
2018-11-19 11:00:01 +0100

    add hero property with name

--- AngularTutorialTourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ AngularTutorialTourOfHeroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+   }
 
+  hero = 'Windstorm';
+  
   ngOnInit() {
   }
 
